abf1eb542339060be29dc6fab6e2d5842b38ef48
Andy Marshall <167748704+andymarshall-35@users.noreply.github.com>
2025-12-09 17:03:26 +0000

    initial screens setup

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,15 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+folder_route_v1 = '/v/1/'
+
+
+router.post('/route-v-1/10', function(request, response) {
+    var value = request.session.data['10']
+    if (value == 'No'){ 
+        response.redirect(folder_route_v1+'12?route=adult')
+    } else {
+        response.redirect(folder_route_v1+'n1?route=child')
+    }
+})
